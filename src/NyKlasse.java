3c8f6f8a8504791653fc40391cba7d607da65858
Jorgen Moen <589688@stud.hvl.no>
2022-02-21 12:00:01 +0100

    andre commit

---
+++ src/NyKlasse.java
@@ -0,0 +1,16 @@
+public class NyKlasse {
+    
+    private int tall;
+    private String streng;
+   
+    public NyKlasse(int tall, String streng) {
+        this.tall = tall;
+        this.streng = streng;
+    }
+
+
+    
+
+
+
+}
